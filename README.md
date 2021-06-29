7d8c1a0b539a7ae0f071c2f91326f45ccac8ab6d
Dongdong Tian <seisman.info@gmail.com>
2021-06-28 22:50:38 -0400

    Improve mklogo.sh and README (#4)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # GMT 中文社区 logo
 
-**下载 Logo：** [PDF 格式](gmt-china-logo.pdf) | [JPG 格式](gmt-china-logo.jpg) | [PNG 格式](gmt-china-logo.png) | [方形版本 PNG格式](gmt-china-logo-square.png)
+**下载 logo：** [PDF 格式](gmt-china-logo.pdf) | [JPG 格式](gmt-china-logo.jpg) | [PNG 格式](gmt-china-logo.png) | [方形版本 PNG 格式](gmt-china-logo-square.png)
 
 ![](gmt-china-logo.png)
--- mklogo.sh
+++ mklogo.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# GMT中文社区logo
+# GMT 中文社区 logo
 #
 gmt begin gmt-china-logo pdf,jpg,PNG
-gmt coast -Rd -JI110/10c -Dc -A10000 -G230/230/230 -S160/175/217
-gmt coast -ECN+g204/204/204 -EHK+g204/204/204 -ETW+g204/204/204 -EMO+g204/204/204
-echo 110 -20 GMT/China | gmt text -F+f35p,19,0/0/100
+	gmt coast -Rd -JI110/10c -Dc -A10000 -G230/230/230 -S160/175/217
+	gmt coast -ECN+g204/204/204 -EHK+g204/204/204 -ETW+g204/204/204 -EMO+g204/204/204
+	echo 110 -20 GMT/China | gmt text -F+f35p,19,0/0/100
 gmt end
 
-# 方形版本的logo
+# 方形版本的 logo
 gmt begin gmt-china-logo-square PNG A+n
-gmt set PS_MEDIA 10cx10c
-gmt coast -Rd -JI110/10c -Dc -A10000 -G230/230/230 -S160/175/217 -Xc -Yc
-gmt coast -ECN+g204/204/204 -EHK+g204/204/204 -ETW+g204/204/204 -EMO+g204/204/204
-echo 110 -20 GMT/China | gmt text -F+f35p,19,0/0/100
+	gmt set PS_MEDIA 10cx10c
+	gmt coast -Rd -JI110/10c -Dc -A10000 -G230/230/230 -S160/175/217 -Xc -Yc
+	gmt coast -ECN+g204/204/204 -EHK+g204/204/204 -ETW+g204/204/204 -EMO+g204/204/204
+	echo 110 -20 GMT/China | gmt text -F+f35p,19,0/0/100
 gmt end
