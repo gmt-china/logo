462141ba1c7e91398297975b3a2e242afe42c5f2
core-man <coreman.seism@gmail.com>
2021-07-07 14:01:29 +0800

    Add GMT and GMT-China combined logo

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # GMT 中文社区 logo
 
-**下载 logo：** [PDF 格式](gmt-china-logo.pdf) | [JPG 格式](gmt-china-logo.jpg) | [PNG 格式](gmt-china-logo.png) | [方形版本 PNG 格式](gmt-china-logo-square.png)
+**GMT 中文社区 logo：** [PDF 格式](gmt-china-logo.pdf) | [JPG 格式](gmt-china-logo.jpg) | [PNG 格式](gmt-china-logo.png) | [方形版本 PNG 格式](gmt-china-logo-square.png)
 
 ![](gmt-china-logo.png)
+
+**GMT 与 GMT 中文社区联合 logo：** [PDF 格式](combined_logo.pdf) | [PNG 格式](gcombined_logo.png)
+![](combined_logo.png)
--- mklogo.sh
+++ mklogo.sh
@@ -2,7 +2,7 @@
 #
 # GMT 中文社区 logo
 #
-gmt begin gmt-china-logo pdf,jpg,PNG
+gmt begin gmt-china-logo pdf,jpg,PNG,eps
 	gmt coast -Rd -JI110/10c -Dc -A10000 -G230/230/230 -S160/175/217
 	gmt coast -ECN+g204/204/204 -EHK+g204/204/204 -ETW+g204/204/204 -EMO+g204/204/204
 	echo 110 -20 GMT/China | gmt text -F+f35p,19,0/0/100
@@ -15,3 +15,12 @@ gmt begin gmt-china-logo-square PNG A+n
 	gmt coast -ECN+g204/204/204 -EHK+g204/204/204 -ETW+g204/204/204 -EMO+g204/204/204
 	echo 110 -20 GMT/China | gmt text -F+f35p,19,0/0/100
 gmt end
+
+# GMT 与 GMT 中文社区联合 logo
+gmt begin combined_logo png,pdf
+gmt basemap -R0/16/0/4 -Jx1c -B+n
+gmt logo -DjTL+w8c
+echo 8.8 2.5 + | gmt text -F+f50p,1,lightred
+gmt image gmt-china-logo.eps -DjTR+w6.25c
+gmt end
+rm gmt-china-logo.eps
