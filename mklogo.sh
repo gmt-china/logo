b9447fd2cc885cce53f4c4eb3252e05bca34ff8a
core-man <coreman.seism@gmail.com>
2021-07-08 22:32:38 +0800

    Fix png to PNG

--- mklogo.sh
+++ mklogo.sh
@@ -2,14 +2,14 @@
 #
 # GMT 中文社区 logo
 #
-gmt begin gmt-china-logo pdf,jpg,png,eps
+gmt begin gmt-china-logo pdf,jpg,PNG,eps
 	gmt coast -Rd -JI110/10c -Dc -A10000 -G230/230/230 -S160/175/217
 	gmt coast -ECN+g204/204/204 -EHK+g204/204/204 -ETW+g204/204/204 -EMO+g204/204/204
 	echo 110 -20 GMT/China | gmt text -F+f35p,19,0/0/100
 gmt end
 
 # 方形版本的 logo
-gmt begin gmt-china-logo-square png A+n
+gmt begin gmt-china-logo-square PNG A+n
 	gmt set PS_MEDIA 10cx10c
 	gmt coast -Rd -JI110/10c -Dc -A10000 -G230/230/230 -S160/175/217 -Xc -Yc
 	gmt coast -ECN+g204/204/204 -EHK+g204/204/204 -ETW+g204/204/204 -EMO+g204/204/204
